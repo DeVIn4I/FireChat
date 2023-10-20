6142bd41b96af4ec44bf055d11558d1b5410c901
DeVInchI <i@dizzyfly.ru>
2023-10-20 15:24:26 +0300

    Initial Commit

---
+++ FireChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FireChat
+//
+//  Created by Razumov Pavel on 20.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
