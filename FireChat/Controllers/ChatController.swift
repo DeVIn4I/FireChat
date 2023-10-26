cc6c88df92e146af5fe9fede917f6531f193f35b
DeVInchI <i@dizzyfly.ru>
2023-10-26 16:01:52 +0300

    Setting Up Message Cells

--- FireChat/Controllers/ChatController.swift
+++ FireChat/Controllers/ChatController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+private let reuseId = "ChatCell"
+
 class ChatController: UICollectionViewController {
     
     // MARK: - Properties
@@ -44,5 +46,29 @@ class ChatController: UICollectionViewController {
     func configureUI() {
         configureNavigationBar(title: user.username, prefersLargeTitle: false)
         view.backgroundColor = .white
+        collectionView.register(MessageCell.self, forCellWithReuseIdentifier: reuseId)
+        collectionView.alwaysBounceVertical = true
+    }
+}
+
+extension ChatController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseId, for: indexPath) as! MessageCell
+        
+        return cell
+    }
+}
+
+extension ChatController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 16, left: 0, bottom: 16, right: 0)
     }
 }
---
+++ FireChat/Views/MessageCell.swift
@@ -0,0 +1,69 @@
+//
+//  MessageCell.swift
+//  FireChat
+//
+//  Created by Razumov Pavel on 26.10.2023.
+//
+
+import UIKit
+
+class MessageCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    private let profileImageView: UIImageView = {
+        let view = UIImageView()
+        view.contentMode = .scaleAspectFill
+        view.clipsToBounds = true
+        view.backgroundColor = .gray
+        return view
+    }()
+    
+    private let textView: UITextView = {
+        let view = UITextView()
+        view.backgroundColor = .clear
+        view.font = .systemFont(ofSize: 16)
+        view.isScrollEnabled = false
+        view.isEditable = false
+        view.textColor = .white
+        view.text = "Test some text . . . "
+        return view
+    }()
+    
+    private let bubbleContainer: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemPurple
+        return view
+    }()
+    
+    // MARK: - Lifecycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(left: leadingAnchor, bottom: bottomAnchor, paddingLeft: 8, paddingBottom: -4)
+        profileImageView.setDimensions(height: 32, width: 32)
+        profileImageView.layer.cornerRadius = 32 / 2
+        
+        addSubview(bubbleContainer)
+        bubbleContainer.layer.cornerRadius = 12
+        bubbleContainer.anchor(top: topAnchor, left: profileImageView.trailingAnchor, paddingLeft: 12)
+        bubbleContainer.widthAnchor.constraint(lessThanOrEqualToConstant: 250).isActive = true
+        
+        bubbleContainer.addSubview(textView)
+        textView.anchor(
+            top: bubbleContainer.topAnchor,
+            left: bubbleContainer.leadingAnchor,
+            bottom: bubbleContainer.bottomAnchor,
+            right: bubbleContainer.trailingAnchor,
+            paddingTop: 4,
+            paddingLeft: 12,
+            paddingBottom: 4,
+            paddingRight: 12
+        )
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
